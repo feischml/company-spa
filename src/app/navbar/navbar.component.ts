d612a325541ce3e371261b4f0c17cad5d6ea4a39
feischml <metallaszlo@yahoo.com>
2018-07-26 22:22:06 +0300

    commit

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { SmoothScrollToDirective } from 'ng2-smooth-scroll';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [ SmoothScrollToDirective ]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scroll: SmoothScrollToDirective = new SmoothScrollToDirective() ) { }
 
   ngOnInit() {
+  }
+
+  scrollTo(divId: String){
+    this.scroll.scrollTo = divId;
+    this.scroll.duration = 1500;
+    this.scroll.offset = 80;
+    // trigger the scroll
+    this.scroll.onClick();
   }
 
 }
